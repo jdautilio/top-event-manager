3e3b0e13f3e736e54a10aca33df01e59b5f5e757
Joel D'Autilio <joeldautilio@gmail.com>
2024-11-10 17:56:58 -0300

    Iteration 3: Using Google's Civic Information

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip, levels: 'country', roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+
+    legislators.officials.map(&:name).join(', ')
+  rescue Google::Apis::ClientError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts 'EventManager initialized.'
 
 contents = CSV.open(
   'event_atendees.csv',
@@ -16,5 +34,7 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
