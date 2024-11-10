b9857d3f80afa7d6e6725b45ba432516f1b5eb13
Joel D'Autilio <joeldautilio@gmail.com>
2024-11-10 18:20:19 -0300

    Iteration 4: Form letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,6 +2,7 @@
 
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -12,14 +13,20 @@ def legislators_by_zipcode(zip)
   civic_info.key = File.read('secret.key').strip
 
   begin
-    legislators = civic_info.representative_info_by_address(
-      address: zip, levels: 'country', roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-
-    legislators.officials.map(&:name).join(', ')
+    civic_info.representative_info_by_address(
+      address: zip, levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
   rescue Google::Apis::ClientError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
+end
+
+def save_thank_you_letter(id, form_letter)
+  filename = "output/thanks_#{id}.html"
+
+  Dir.mkdir 'output' unless Dir.exist? 'output'
+  File.open(filename, 'w') { |file| file.puts form_letter }
 end
 
 puts 'EventManager initialized.'
@@ -29,12 +36,17 @@ contents = CSV.open(
   headers: true,
   header_converters: :symbol
 )
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
 end
